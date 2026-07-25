f3f7cfce8248572b526f3331b1b92b9f2a8d64bf
Diego Costa <diego@diegocosta.me>
2026-07-25 00:25:06 -0300

    chore: changed page domain

--- components/NotFound/index.tsx
+++ components/NotFound/index.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.css';
 export default function NotFound({ pathname }: { pathname: string }) {
   return (
     <div className={styles.container}>
-      <CodeBlock className="language-plain">{`$ open https://diegocosta.me${pathname}<br />I&apos;m sorry, but the page you&apos;re looking for cannot be found.`}</CodeBlock>
+      <CodeBlock className="language-plain">{`$ open https://diegocosta.com.br${pathname}<br />I&apos;m sorry, but the page you&apos;re looking for cannot be found.`}</CodeBlock>
     </div>
   );
 }
