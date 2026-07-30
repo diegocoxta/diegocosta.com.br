cc42c15b96d8f073e88717be1dce8e86d59f4ff8
Diego Costa <diego@diegocosta.me>
2026-07-30 16:13:18 -0300

    Remove Divisor component from HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Layout from '~/components/Layout';
 import AboutMe from '~/components/AboutMe';
-import Divisor from '~/components/Divisor';
 
 import { getPages, getPosts, profile } from '~/app/cms';
 
@@ -8,7 +7,6 @@ export default function HomePage() {
   return (
     <Layout repository={profile.repository.url} author={profile.author} posts={getPosts()} pages={getPages()} centered>
       <AboutMe bio={profile.bio} links={profile.links} />
-      <Divisor />
     </Layout>
   );
 }
