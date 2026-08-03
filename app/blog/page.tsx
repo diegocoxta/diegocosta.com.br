5e3509619f37e59384911a4863f1a4df38821c64
Diego Costa <diego@diegocosta.me>
2026-08-02 23:12:09 -0300

    Add PageName component to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Divisor from '~/components/Divisor';
 import Links from '~/components/Links';
 import Container from '~/components/Container';
+import PageName from '~/components/TagName';
 import Title from '~/components/Title';
 import Attributes from '~/components/Attributes';
 import Article from '~/components/Article';
@@ -14,6 +15,7 @@ export default function HomePage() {
   return (
     <Layout repository={profile.repository.url} author={profile.author} posts={getPosts()} pages={getPages()}>
       <Container>
+        <PageName>blog</PageName>
         {getPosts().map((post, index: number) => (
           <article key={`article-${index}`}>
             <Title>
