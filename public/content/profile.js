7bb2db53a3b8679ad47bfd7a1907bf4b27c6f33e
Diego Costa <diego@diegocosta.me>
2026-08-07 17:59:54 -0300

    Update email contact in profile links

--- public/content/profile.js
+++ public/content/profile.js
@@ -10,7 +10,7 @@ const profile = {
     { label: 'Linkedin', url: 'https://linkedin.com/in/diegocoxta' },
     { label: 'Github', url: 'https://github.com/diegocoxta' },
     { label: 'Blog', url: '/blog' },
-    { label: 'E-mail', url: 'mailto:contato@diegocosta.com.br' }
+    { label: 'E-mail', url: 'mailto:diego@diegocosta.com.br' }
   ],
   repository: npmPackage.repository,
 };
